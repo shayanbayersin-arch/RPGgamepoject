c39fb393d8ef698a2e785935ceecfa511b31d9d7
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-05-16 20:06:23 +0500

    feat(combat): created the AttackStrategy interface to implement the Strategy pattern

--- core/src/main/java/com/batyrlegacy/game/combat/AttackStrategy.java
+++ core/src/main/java/com/batyrlegacy/game/combat/AttackStrategy.java
@@ -1,4 +1,10 @@
 package com.batyrlegacy.game.combat;
 
-public class AttackStrategy {
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+
+public interface AttackStrategy {
+    Animation<TextureRegion> getAnimation();
+    float getDamage();
+    float getDuration(); // Сколько длится взмах/выстрел
 }
