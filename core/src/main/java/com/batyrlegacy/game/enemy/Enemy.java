4fb416e7b9f04f0bc863d097c72cb2aecb693ba0
shayanbayersin-arch <shayanbayersin@gmail.com>
2026-05-16 20:10:42 +0500

    feat(enemy): create Enemy base model with health, stats and animations

--- core/src/main/java/com/batyrlegacy/game/enemy/Enemy.java
+++ core/src/main/java/com/batyrlegacy/game/enemy/Enemy.java
@@ -1,4 +1,33 @@
 package com.batyrlegacy.game.enemy;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
+
 public class Enemy {
+    public Vector2 pos = new Vector2();
+    public float maxHp;
+    public float currentHp;
+    public float scale;
+    public float damage;
+
+    public Animation<TextureRegion> idleAnim;
+    public Animation<TextureRegion> runAnim;
+    public Animation<TextureRegion> attackAnim;
+
+    public boolean isMoving = false;
+    public boolean isAttacking = false;
+    public float attackTime = 0f;
+    public boolean hitRegistered = false;
+
+    public Enemy(float maxHp, float scale, float damage,
+                 Animation<TextureRegion> idle, Animation<TextureRegion> run, Animation<TextureRegion> attack) {
+        this.maxHp = maxHp;
+        this.currentHp = maxHp;
+        this.scale = scale;
+        this.damage = damage;
+        this.idleAnim = idle;
+        this.runAnim = run;
+        this.attackAnim = attack;
+    }
 }
